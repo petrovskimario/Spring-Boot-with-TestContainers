a4f1f1f798f351e679efe565d73e70d9f147ec9e
Mario Petrovski <marivce12@gmail.com>
2024-08-07 10:30:53 +0200

    Add logging req/res for validation service

--- src/main/java/com/iwlabs/testcontainers/validator/ValidateGpaHttpService.java
+++ src/main/java/com/iwlabs/testcontainers/validator/ValidateGpaHttpService.java
@@ -2,11 +2,13 @@ package com.iwlabs.testcontainers.validator;
 
 import com.iwlabs.testcontainers.validator.input.StudentValidationInput;
 import com.iwlabs.testcontainers.validator.output.StudentValidationOutput;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
+@Slf4j
 public class ValidateGpaHttpService {
 
     @Autowired
@@ -17,11 +19,13 @@ public class ValidateGpaHttpService {
     }
 
     public StudentValidationOutput callStudentValidationService(StudentValidationInput input) {
+        log.info("Sending request to validate GPA: {}", input);
         return webClient.post()
                 .uri("/api/v1/validateGpa")
                 .bodyValue(input)
                 .retrieve()
                 .bodyToMono(StudentValidationOutput.class)
+                .doOnNext(res -> log.info("Received response from validate GPA: {}", res))
                 .block();
     }
 }
